8e59e1ecbff1f9ead06c00cd71c93e006a11791d
Matheus Aroxa <matheusaroxa13@gmail.com>
2024-11-09 08:43:32 -0300

    Implemented basic auth

--- src/main/java/com/miromorii/springsecurityfirstproject/controller/StudentController.java
+++ src/main/java/com/miromorii/springsecurityfirstproject/controller/StudentController.java
@@ -12,11 +12,11 @@ import java.util.List;
 @RequestMapping(value = "api/v1/students")
 public class StudentController {
 
-    private List<Student> students = List.of(new Student(1, "Evellyn beatriz"),
+    private final List<Student> students = List.of(new Student(1, "Evellyn beatriz"),
                                                 new Student(2, "marcos"),
                                                 new Student(3, "pedro sampaio"));
 
-    @GetMapping("{studentId}")
+    @GetMapping(value = "{studentId}")
     public Student getStudent(@PathVariable("studentId") Integer id){ //refatorar para findById()
         return students.stream()
                 .filter(student -> id.equals(student.getId()))
