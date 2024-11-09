ae39c37febe2b150aff32eab3a806ad3076b3c3e
Matheus Aroxa <matheusaroxa13@gmail.com>
2024-11-09 07:02:54 -0300

    initial configuration

---
+++ src/test/java/com/miromorii/springsecurityfirstproject/SpringSecurityFirstProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miromorii.springsecurityfirstproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityFirstProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
